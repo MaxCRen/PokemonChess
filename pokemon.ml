5affaf5bba9034bc723f076a771d05962140d91e
Dan Marzagalli <dbm235@cornell.edu>
2018-10-23 21:55:26 -0400

    json schema and from_json in pokemon.mli

--- pokemon.ml
+++ pokemon.ml
@@ -1,11 +1,12 @@
 open Moves
 open Ptype
+open Yojson.Basic.Util
 
 
 type t = {
   name: string;
   pokeType: (Ptype.t * Ptype.t option);
-  moveSet: Moves.t list;
+  moveSet: Yojson.Basic.json list;
   (*contains the multiplier for attributes, it is initializes as all 1's*)
   attr_mult: int list;
   (* attributes [hp; attack; defense; speed] *)
@@ -26,3 +27,20 @@ let make_pokemon n typ mset attr stat = {
   confused = false;
   accuracy = 1.
 }
+
+let from_json json =
+  let name = json |> member "name" |> to_string in
+  let ptype = json |> member "ptype" |> to_list |> List.map (to_string) in 
+  let moves = json |> member "moves" |> to_list in
+  let attr = json |> member "stats" |> to_list |> List.map (to_int) in  
+  (* there are max of 2 types, and we parse from a list of types ...
+   * either there's one or 2, so we match on these *)
+  match ptype with 
+  | [] -> failwith "Error"
+  | h::[] -> 
+    make_pokemon name 
+      (Ptype.makeType h [], None) moves attr None
+  | h::t::[] ->  
+    make_pokemon name 
+      (Ptype.makeType h [], Some (Ptype.makeType t [])) moves attr None
+  | _ -> failwith "Error"
--- pokemon.mli
+++ pokemon.mli
@@ -1,9 +1,10 @@
 open Ptype
 open Moves
+open Yojson.Basic.Util
 (*Our representaion type of a pokemon*)
 type t
 
 (*[make_pokemon name poketype moveset attributes status] creates a our representation
 of a pokmon type using the given arguments*)
 val make_pokemon: string -> (Ptype.t * Ptype.t option) -> 
-                            Moves.t list -> int list -> Moves.status option -> t
+                            Yojson.Basic.json list -> int list -> Moves.status option -> t
