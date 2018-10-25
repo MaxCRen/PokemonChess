a062260f386e00803e077d21947977ea637a83cf
Dan Marzagalli <dbm235@cornell.edu>
2018-10-24 22:22:03 -0400

    ui

--- main.ml
+++ main.ml
@@ -37,7 +37,7 @@ let opponent_move bat =
   let r = Random.int (List.length opponent_moves) in 
   let move = List.nth opponent_moves r in 
   if can_use_move bat (Moves.get_name move) then 
-    (print_string (op_name^" used "^(Moves.get_name move)^"\n\n\n");
+    ANSITerminal.(print_string [red](op_name^" used "^(Moves.get_name move)^"\n\n\n");
     Battle.use_move bat move)
   else print_string "Can Not Use Move\n\n\n\n" 
 
@@ -45,8 +45,8 @@ let rec display_moves moves =
   match moves with
   |[] -> print_string("\n")
   |h::t when ((List.length t) mod 2 = 0) -> 
-                print_string("\n\t"^Moves.get_name h); (display_moves t); 
-  |h::t ->  print_string("\t\t\t"^Moves.get_name h); (display_moves t)
+    ANSITerminal.(print_string[green]("\n\t"^Moves.get_name h)); (display_moves t); 
+  |h::t ->  ANSITerminal.(print_string[green]("\t\t\t"^Moves.get_name h)); (display_moves t)
 
 (** [hp_display number pokemon] returns a rough percentage of [number] '*'
     characters that is equivalent to (current_health / max_health) of 
@@ -64,18 +64,19 @@ let printed bat =
   print_string("__________________________________________________________\n");
   let opponent = Battle.get_opponent bat in
   let player = Battle.get_player bat in
-  print_string("Pokemon Battle: \nOpponent's Pokemon: \n"); 
-  print_string(Pokemon.get_name opponent ^ "\t Health:" ^ 
-               (hp_display 50. opponent));
+  print_string("Pokemon Battle:");
+  ANSITerminal.(print_string[red](" \nOpponent's Pokemon: \n")); 
+  ANSITerminal.(print_string[red](Pokemon.get_name opponent ^ "\t Health:" ^ 
+               (hp_display 50. opponent)));
   let opp_health = get_curr_hp opponent |> Pervasives.string_of_int in
   let opp_max_health = get_max_health opponent |> Pervasives.string_of_int in
-  print_string(opp_health^"/"^opp_max_health^" hp\nMoves:");
-  print_string("\n\n\n\nPlayer's Pokemon:\n");
-  print_string(Pokemon.get_name player ^ "\t Health: " ^ 
-               (hp_display 50. player));
+  ANSITerminal.(print_string[red](opp_health^"/"^opp_max_health^" hp\nMoves:"));
+  ANSITerminal.(print_string[green]("\n\n\n\nPlayer's Pokemon:\n"));
+  ANSITerminal.(print_string [green] (Pokemon.get_name player ^ "\t Health: " ^ 
+               (hp_display 50. player)));
   let curr_health = get_curr_hp player |> Pervasives.string_of_int in
   let max_health = get_max_health player |> Pervasives.string_of_int in
-  print_string(curr_health^"/"^max_health^" hp\nMoves:");
+  ANSITerminal.(print_string[green](curr_health^"/"^max_health^" hp\nMoves:"));
   get_moves player |> display_moves;
   print_string("__________________________________________________________\n")
 
@@ -92,7 +93,7 @@ let use_move bat str =
   let poke_name = bat |> Battle.get_player |> Pokemon.get_name in
   let move_list = bat |> Battle.get_player |> get_moves in
   let move = get_move_from_str move_list str in
-  print_string (poke_name^" used "^(Moves.get_name move)^"\n\n\n");
+  ANSITerminal.(print_string[green] (poke_name^" used "^(Moves.get_name move)^"\n\n\n"));
   Battle.use_move bat move
   else print_string "Can Not Use Move\n\n\n\n" 
 
