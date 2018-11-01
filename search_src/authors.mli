8956c7e9dcf045ba401e12abb13a1ff5ff7ff258
Dan Marzagalli <dbm235@cornell.edu>
2018-10-31 21:56:08 -0400

    removed stuff too

--- search_src/authors.mli
+++
@@ -1,9 +0,0 @@
-(**
-  @author Max Ren (mcr229), Genghis Shyy (gs484), Daniel Marzagalli (dbm235)
-
- *)
-
- (** [hours_worked] is a list of the number of hours each team member worked
-      on this assignment *)
-
-val hours_worked : int list
